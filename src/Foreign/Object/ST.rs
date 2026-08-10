5d7d4da18352e9965773f411b3dd05dc3ba378b7
0x1 <kevin.francart3@gmail.com>
2026-08-10 16:48:06 +0200

    misc

---
+++ src/Foreign/Object/ST.rs
@@ -0,0 +1,5 @@
+pub fn Foreign_Object_ST_new() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Foreign_Object_ST_peekImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Foreign_Object_ST_poke() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Foreign_Object_ST_delete() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub type STObject = crate::UnknownType;
