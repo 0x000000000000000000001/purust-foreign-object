15db3e315c06cbbf1df70b63ac183e1d17f39f84
0x1 <kevin.francart3@gmail.com>
2026-08-12 09:18:49 +0200

    misc

--- src/Foreign/Object.rs
+++ src/Foreign/Object.rs
@@ -1,14 +1,14 @@
-pub fn Foreign_Object__copyST() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object_runST() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object__fmapObject() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object__foldM() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object__foldSCObject() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object_all() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object_empty() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object_size() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object__lookup() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object__lookupST() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object_toArrayWithKey() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object_keys() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Foreign_Object__mapWithKey() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Foreign_Object__copyST() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object_runST() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object__fmapObject() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object__foldM() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object__foldSCObject() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object_all() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object_empty() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object_size() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object__lookup() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object__lookupST() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object_toArrayWithKey() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object_keys() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
+pub fn Foreign_Object__mapWithKey() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
 pub type Object = crate::UnknownType;
