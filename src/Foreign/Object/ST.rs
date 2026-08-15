f6141942d9a9c317e590d0174192739ee857f4ff
0x1 <kevin.francart3@gmail.com>
2026-08-15 19:05:15 +0200

    misc

--- src/Foreign/Object/ST.rs
+++
@@ -1,5 +0,0 @@
-pub fn Foreign_Object_ST_new() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Foreign_Object_ST_peekImpl() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Foreign_Object_ST_poke() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Foreign_Object_ST_delete() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub type STObject = crate::UnknownType;
